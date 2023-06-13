338e790e4d6d0a6981a6856bbae0b3a3827a2aeb
Phạm Xuân Huy <110225257+zacky319@users.noreply.github.com>
2023-06-13 18:47:56 +0700

    Add files via upload

--- hrms.sql
+++ hrms.sql
@@ -39,7 +39,12 @@ CREATE TABLE GrantedPermission (
   FOREIGN KEY (role_id) REFERENCES Roles(role_id),
   FOREIGN KEY (permission_id) REFERENCES Permission(permission_id)
 );
-
+-- Create detail_tax_income table
+CREATE TABLE detail_tax_income (
+  detail_tax_income_id INT PRIMARY KEY,
+  muc_chiu_thue FLOAT,
+  thue_suat FLOAT
+);
 -- Create Experience table
 CREATE TABLE Experience (
   experience_id INT PRIMARY KEY,
@@ -49,7 +54,48 @@ CREATE TABLE Experience (
   end_date DATE,
   tech_stack NVARCHAR(500)
 );
+-- Create Skill table
+CREATE TABLE Skill (
+  skill_id INT PRIMARY KEY,
+  skill_name NVARCHAR(100),
+  skill_description NVARCHAR(500)
+);
+-- Create Deduction table
+CREATE TABLE Deduction (
+  deduction_id INT PRIMARY KEY,
+  deduction_type NVARCHAR,
+  amount DECIMAL(18, 2)
+);
 
+-- Create Allowances table
+CREATE TABLE Allowances (
+  allowance_id INT PRIMARY KEY,
+  allowance_type NVARCHAR(200),
+  amount DECIMAL(18, 2)
+);
+-- Create Skill_employee table
+CREATE TABLE Skill_employee (
+  unique_id INT PRIMARY KEY,
+  employee_id INT,
+  level NVARCHAR(50),
+  skill_id INT,
+  FOREIGN KEY (employee_id) REFERENCES Employee(employee_id),
+  FOREIGN KEY (skill_id) REFERENCES Skill(skill_id)
+);
+
+-- Create Job table
+CREATE TABLE Job (
+  job_id INT PRIMARY KEY,
+  job_title NVARCHAR(200),
+  job_description NVARCHAR(500),
+  start_date DATE,
+  --end_date DATE,
+  status NVARCHAR(200),
+  base_salary_per_hour DECIMAL(18, 2),
+  allowance_id INT,
+  bonus DECIMAL(18, 2),
+  FOREIGN KEY (allowance_id) REFERENCES Allowances(allowance_id)
+);
 -- Create Employee table
 CREATE TABLE Employee (
   employee_id INT PRIMARY KEY,
@@ -113,12 +159,7 @@ CREATE TABLE Overtime (
   FOREIGN KEY (employee_id) REFERENCES Employee(employee_id)
 );
 
--- Create detail_tax_income table
-CREATE TABLE detail_tax_income (
-  detail_tax_income_id INT PRIMARY KEY,
-  muc_chiu_thue FLOAT,
-  thue_suat FLOAT
-);
+
 
 -- Create PaySlip table
 CREATE TABLE PaySlip (
@@ -147,36 +188,8 @@ CREATE TABLE PaySlip (
   FOREIGN KEY (allowances_id) REFERENCES EmployeeBenefit(allowances_id)
 );
 
--- Create Skill table
-CREATE TABLE Skill (
-  skill_id INT PRIMARY KEY,
-  skill_name NVARCHAR(100),
-  skill_description NVARCHAR(500)
-);
 
--- Create Skill_employee table
-CREATE TABLE Skill_employee (
-  unique_id INT PRIMARY KEY,
-  employee_id INT,
-  level NVARCHAR(50),
-  skill_id INT,
-  FOREIGN KEY (employee_id) REFERENCES Employee(employee_id),
-  FOREIGN KEY (skill_id) REFERENCES Skill(skill_id)
-);
 
--- Create Job table
-CREATE TABLE Job (
-  job_id INT PRIMARY KEY,
-  job_title NVARCHAR(200),
-  job_description NVARCHAR(500),
-  start_date DATE,
-  --end_date DATE,
-  status NVARCHAR(200),
-  base_salary_per_hour DECIMAL(18, 2),
-  allowance_id INT,
-  bonus DECIMAL(18, 2),
-  FOREIGN KEY (allowance_id) REFERENCES Allowances(allowance_id)
-);
 
 -- Create EmployeeLoanLog table
 CREATE TABLE EmployeeLoanLog (
@@ -193,12 +206,6 @@ CREATE TABLE EmployeeLoanLog (
   FOREIGN KEY (employee_id) REFERENCES Employee(employee_id)
 );
 
--- Create Deduction table
-CREATE TABLE Deduction (
-  deduction_id INT PRIMARY KEY,
-  deduction_type NVARCHAR,
-  amount DECIMAL(18, 2)
-);
 
 -- Create DeductionSumary table
 CREATE TABLE DeductionSumary (
@@ -209,12 +216,7 @@ CREATE TABLE DeductionSumary (
   FOREIGN KEY (payslip_id) REFERENCES PaySlip(payslip_id)
 );
 
--- Create Allowances table
-CREATE TABLE Allowances (
-  allowance_id INT PRIMARY KEY,
-  allowance_type NVARCHAR(200),
-  amount DECIMAL(18, 2)
-);
+
 
 -- Create Attendance table
 CREATE TABLE Attendance (
