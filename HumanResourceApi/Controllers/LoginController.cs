a383243e5dce983536f9f03a5de4f04964ab8617
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2023-06-21 22:20:16 +0700

    implement jwtBearer, generate token when login

---
+++ HumanResourceApi/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using HumanResourceApi.DTO.Users;
+using HumanResourceApi.Models;
+using HumanResourceApi.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace HumanResourceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly UserRepo _userRepo;
+        private readonly IMapper _mapper;
+
+        public LoginController(IConfiguration configuration, UserRepo userRepo, IMapper mapper)
+        {
+            _config = configuration;
+            _userRepo = userRepo;
+            _mapper = mapper;
+        }
+
+        private string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        //Check if valid user -> generate token
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult CheckLogin([FromBody] LoginDto loginInfo)
+        {
+            try
+            {
+                IActionResult response = Unauthorized();
+                if (loginInfo == null)
+                    return BadRequest(ModelState);
+
+                var user_ = _userRepo.CheckLogin(loginInfo);
+
+                if (user_ == null)
+                {
+                    return response;
+                }
+                
+                var token = GenerateToken(user_);
+
+                response = Ok(new { token = token });
+                return response;
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
