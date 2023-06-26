d82447ab267cd8c9170137d527b965f7bf903661
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2023-06-26 21:03:37 +0700

    Add swagger service for testing api with token on swagger

---
+++ HumanResourceApi/SwaggerService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace HumanResourceApi;
+
+public static class SwaggerService
+{
+    public static void AddSwaggerService(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(
+            c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo { Title = "Management Api", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+                //Enable api summary
+                c.EnableAnnotations();
+            }
+        );
+    }
+}
