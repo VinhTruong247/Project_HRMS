7d593ddf3af0515cff43bfd6394144a5ff7eff34
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2023-06-26 11:08:23 +0700

    test

--- HumanResourceApi/Controllers/UserController.cs
+++ HumanResourceApi/Controllers/UserController.cs
@@ -21,7 +21,7 @@ namespace HumanResourceApi.Controllers
             _userRepo = userRepo;
         }
 
-
+        [Authorize]
         [HttpGet("get/users")]
         public IActionResult GetAll()
         {
