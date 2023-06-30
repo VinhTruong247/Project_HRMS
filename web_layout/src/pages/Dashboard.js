402aa3ceb0ad6cb9cae23639d949275a7de9d849
Zacky <zackwalker130903@gmail.com>
2023-06-30 08:39:29 +0700

    Update Dashboard with content grid

--- web_layout/src/pages/Dashboard.js
+++ web_layout/src/pages/Dashboard.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-const Dasboard = () => {
-    return (
-        <div>
-            <h1>
-                GeeksforGeeks is a Computer
-                Science portal for geeks.
-            </h1>
-        </div>
-    );
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-content">
+        <div className="box big-box">1</div>
+        <div className="box small-box">2</div>
+        <div className="box small-box">3</div>
+        <div className="box small-box">4</div>
+        <div className="box small-box">5</div>
+        <div className="box small-box">6</div>
+        <div className="box small-box">7</div>
+        <div className="box big-box">8</div>
+        <div className="box small-box">9</div>
+        <div className="box small-box">10</div>
+      </div>
+    </div>
+  );
 };
- 
-export default Dasboard;
+
+export default Dashboard;
