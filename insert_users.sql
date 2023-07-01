f6583134e5a83144ad38305dec5224d872f07792
Phạm Xuân Huy <110225257+zacky319@users.noreply.github.com>
2023-07-01 10:49:42 +0700

    Update insert_users.sql

--- insert_users.sql
+++ insert_users.sql
@@ -20,31 +20,31 @@ VALUES
   ('RL000003', 'HR_Staff', 'active'),
   ('RL000004', 'Employee', 'active');
 
-INSERT INTO Users (user_id, username, password, Email, role_id, status)
+INSERT INTO Users (user_id, employee_id username, password, Email, role_id, status)
 VALUES
-  ('US000001', N'Hồ', N'P@ssw0rd123!', 'ho.nguyen@example.com', 'RL000004', 1),
-  ('US000002', N'Mai', N'abc123', 'mai.tran@example.com', 'RL000004', 1),
-  ('US000003', N'Thắm', N'Str0ngP@ssw0rd', 'tham.le@example.com', 'RL000004', 1),
-  ('US000004', N'Nam', N'password123', 'nam.tran@example.com', 'RL000004', 1),
-  ('US000005', N'Bình', N'123456789', 'binh.le@example.com', 'RL000004', 1),
-  ('US000006', N'Lan', N'P@ssw0rd789!', 'lan.vu@example.com', 'RL000002', 1),
-  ('US000007', N'Minh', N'abc123', 'minh.tran@example.com', 'RL000003', 1),
-  ('US000008', N'An', N'Str0ngP@ssw0rd', 'an.le@example.com', 'RL000001', 1);
+  ('US000001','EP000001', N'Hồ', N'P@ssw0rd123!', 'ho.nguyen@example.com', 'RL000004', 1),
+  ('US000002','EP000002', N'Mai', N'abc123', 'mai.tran@example.com', 'RL000004', 1),
+  ('US000003','EP000003', N'Thắm', N'Str0ngP@ssw0rd', 'tham.le@example.com', 'RL000004', 1),
+  ('US000004','EP000004', N'Nam', N'password123', 'nam.tran@example.com', 'RL000004', 1),
+  ('US000005','EP000005', N'Bình', N'123456789', 'binh.le@example.com', 'RL000004', 1),
+  ('US000006','EP000006', N'Lan', N'P@ssw0rd789!', 'lan.vu@example.com', 'RL000002', 1),
+  ('US000007','EP000007', N'Minh', N'abc123', 'minh.tran@example.com', 'RL000003', 1),
+  ('US000008','EP000008', N'An', N'Str0ngP@ssw0rd', 'an.le@example.com', 'RL000001', 1);
 
 
 
 -- Insert 13 employees
 
-INSERT INTO Employee (employee_id, first_name, last_name, employee_image, date_of_birth, employee_address, email, phone_number, BankAccountNumber, BankAccountName, BankName, experience_id, user_id, job_id, department_id,status)
+INSERT INTO Employee (employee_id, first_name, last_name, employee_image, date_of_birth, employee_address, email, phone_number, BankAccountNumber, BankAccountName, BankName, job_id, department_id,status)
 VALUES
-  ('EP000001', N'Hồ', N'Nguyễn', 'ho_nguyen.jpg', '1990-05-15', '123 Main St, City, Country', 'ho.nguyen@example.com', '+1234567890', '123456789', N'Hồ Nguyễn', 'Bank XYZ', 'EX000001', 'US000001', 'JB000001', 'DP000001','active'),
-  ('EP000002', N'Mai', N'Trần', 'mai_tran.jpg', '1992-09-20', '456 Elm St, City, Country', 'mai.tran@example.com', '+0987654321', '987654321', N'Mai Trần', 'Bank ABC', 'EX000002', 'US000002', 'JB000001', 'DP000001','active'),
-  ('EP000003', N'Thắm', N'Lê', 'tham_le.jpg', '1988-07-10', '789 Oak St, City, Country', 'tham.le@example.com', '+1357924680', '246813579', N'Thắm Lê', 'Bank DEF', 'EX000003', 'US000003', 'JB000001', 'DP000001','active'),
-  ('EP000004', N'Bình', N'Lê', 'binh_le.jpg', '1991-11-05', '654 Cedar St, City, Country', 'binh.le@example.com', '+0123456789', '123456789', N'Bình Lê', 'Bank ABC', 'EX000004', 'US000005', 'JB000004', 'DP000001','active'),
-  ('EP000005', N'Nam', N'Trần', 'nam_tran.jpg', '1995-03-25', '321 Pine St, City, Country', 'nam.tran@example.com', '+5678901234', '987654321', N'Nam Trần', 'Bank XYZ', 'EX000003', 'US000004', 'JB000002', 'DP000002','active'),
-  ('EP000006', N'Lan', N'Vũ', 'lan_vu.jpg', '1993-12-30', '987 Maple St, City, Country', 'lan.vu@example.com', '+9876543210', '246813579', 'Lan Vũ', N'Bank DEF', 'EX000005', 'US000006', 'JB000005', 'DP000006','active'),
-  ('EP000007', N'Minh', N'Trần', 'minh_tran.jpg', '1989-08-17', '147 Birch St, City, Country', 'minh.tran@example.com', '+5432109876', '987654321', N'Minh Trần', 'Bank XYZ', 'EX000005', 'US000007', 'JB000005', 'DP000006','active'),
-  ('EP000008', N'An', N'Lê', 'an_le.jpg', '1994-06-12', '258 Walnut St, City, Country', 'an.le@example.com', '+7654321098', '123456789', N'An Lê', 'Bank ABC','EX000012', 'US000008', 'JB000012', 'DP000004','active');
+  ('EP000001', N'Hồ', N'Nguyễn', 'ho_nguyen.jpg', '1990-05-15', '123 Main St, City, Country', 'ho.nguyen@example.com', '+1234567890', '123456789', N'Hồ Nguyễn', 'Bank XYZ', 'JB000001', 'DP000001','Active'),
+  ('EP000002', N'Mai', N'Trần', 'mai_tran.jpg', '1992-09-20', '456 Elm St, City, Country', 'mai.tran@example.com', '+0987654321', '987654321', N'Mai Trần', 'Bank ABC','JB000001', 'DP000001','Active'),
+  ('EP000003', N'Thắm', N'Lê', 'tham_le.jpg', '1988-07-10', '789 Oak St, City, Country', 'tham.le@example.com', '+1357924680', '246813579', N'Thắm Lê', 'Bank DEF','JB000001', 'DP000001','Active'),
+  ('EP000005', N'Bình', N'Lê', 'binh_le.jpg', '1991-11-05', '654 Cedar St, City, Country', 'binh.le@example.com', '+0123456789', '123456789', N'Bình Lê', 'Bank ABC','JB000004', 'DP000001','Active'),
+  ('EP000004', N'Nam', N'Trần', 'nam_tran.jpg', '1995-03-25', '321 Pine St, City, Country', 'nam.tran@example.com', '+5678901234', '987654321', N'Nam Trần', 'Bank XYZ','JB000002', 'DP000002','Active'),
+  ('EP000006', N'Lan', N'Vũ', 'lan_vu.jpg', '1993-12-30', '987 Maple St, City, Country', 'lan.vu@example.com', '+9876543210', '246813579', 'Lan Vũ', N'Bank DEF','JB000005', 'DP000006','Active'),
+  ('EP000007', N'Minh', N'Trần', 'minh_tran.jpg', '1989-08-17', '147 Birch St, City, Country', 'minh.tran@example.com', '+5432109876', '987654321', N'Minh Trần', 'Bank XYZ','JB000005', 'DP000006','Active'),
+  ('EP000008', N'An', N'Lê', 'an_le.jpg', '1994-06-12', '258 Walnut St, City, Country', 'an.le@example.com', '+7654321098', '123456789', N'An Lê', 'Bank ABC','JB000012', 'DP000004','Active');
 
 -- Insert 13 jobs
 INSERT INTO Job (job_id, job_title, job_description, start_date, status, base_salary_per_hour, allowance_id, bonus)
@@ -74,21 +74,21 @@ VALUES
   ('DP000007', 'Cybersecurity', 'Responsible for protecting the company''s IT systems and data from security threats.', 'Active'),
   ('DP000008', 'Business Analysis', 'Responsible for analyzing business requirements and recommending IT solutions.', 'Active');
 
-  INSERT INTO Experience (experience_id, name_project, team_size, start_date, end_date, tech_stack,status)
+  INSERT INTO Experience (experience_id, employee_id, name_project, team_size, start_date, end_date, tech_stack)
 VALUES
-  ('EX000001', 'Website Redesign', 6, '2021-11-15', '2022-03-31', 'HTML, CSS, JavaScript', 'Active'),
-  ('EX000002', 'Mobile App Development', 8, '2022-03-01', '2022-08-31', 'React Native, Firebase, TypeScript', 'Active'),
-  ('EX000003', 'Data Analytics Platform', 5, '2022-09-01', '2023-02-28', 'Python, SQL, Tableau', 'Active'),
-  ('EX000004', 'E-commerce Website Development', 8, '2022-02-15', '2022-07-31', 'PHP, Laravel, MySQL', 'Active'),
-  ('EX000005', 'Mobile App Development', 6, '2021-10-01', '2022-02-28', 'Swift, iOS, Firebase', 'Active'),
-  ('EX000006', 'Data Analysis and Visualization', 4, '2022-03-15', '2022-06-30', 'Python, Pandas, Matplotlib', 'Active'),
-  ('EX000007', 'UI/UX Design for Web Applications', 3, '2022-06-01', '2022-08-31', 'Sketch, Adobe XD, InVision', 'Active'),
-  ('EX000008', 'Blockchain Implementation', 5, '2021-12-01', '2022-04-30', 'Solidity, Ethereum, Truffle', 'Active'),
-  ('EX000009', 'Cloud Infrastructure Deployment', 10, '2022-01-15', '2022-09-30', 'AWS, Azure, Terraform', 'Active'),
-  ('EX000010', 'Machine Learning Model Development', 7, '2022-05-01', '2022-11-30', 'Python, scikit-learn, TensorFlow', 'Active'),
-  ('EX000011', 'CRM System Integration', 4, '2021-09-01', '2022-01-31', 'Java, Spring Boot, Salesforce', 'Active'),
-  ('EX000012', 'Chatbot Development', 3, '2022-03-01', '2022-05-31', 'Python, NLTK, Dialogflow', 'Active'),
-  ('EX000013', 'Game Development', 6, '2021-11-01', '2022-07-31', 'Unity, C#, 3D Modeling', 'Active');
+  ('EX000001','EP000001', 'Website Redesign', 6, '2021-11-15', '2022-03-31', 'HTML, CSS, JavaScript'),
+  ('EX000002','EP000002', 'Mobile App Development', 8, '2022-03-01', '2022-08-31', 'React Native, Firebase, TypeScript'),
+  ('EX000003','EP000003', 'Data Analytics Platform', 5, '2022-09-01', '2023-02-28', 'Python, SQL, Tableau'),
+  ('EX000004','EP000004', 'E-commerce Website Development', 8, '2022-02-15', '2022-07-31', 'PHP, Laravel, MySQL'),
+  ('EX000005','EP000005', 'Mobile App Development', 6, '2021-10-01', '2022-02-28', 'Swift, iOS, Firebase'),
+  ('EX000006','EP000006', 'Data Analysis and Visualization', 4, '2022-03-15', '2022-06-30', 'Python, Pandas, Matplotlib'),
+  ('EX000007','EP000007', 'UI/UX Design for Web Applications', 3, '2022-06-01', '2022-08-31', 'Sketch, Adobe XD, InVision'),
+  ('EX000008','EP000008', 'Blockchain Implementation', 5, '2021-12-01', '2022-04-30', 'Solidity, Ethereum, Truffle'),
+  ('EX000009','EP000001', 'Cloud Infrastructure Deployment', 10, '2022-01-15', '2022-09-30', 'AWS, Azure, Terraform'),
+  ('EX000010','EP000002', 'Machine Learning Model Development', 7, '2022-05-01', '2022-11-30', 'Python, scikit-learn, TensorFlow'),
+  ('EX000011','EP000003', 'CRM System Integration', 4, '2021-09-01', '2022-01-31', 'Java, Spring Boot, Salesforce'),
+  ('EX000012','EP000004', 'Chatbot Development', 3, '2022-03-01', '2022-05-31', 'Python, NLTK, Dialogflow'),
+  ('EX000013','EP000005', 'Game Development', 6, '2021-11-01', '2022-07-31', 'Unity, C#, 3D Modeling');
  
 -- Insert 10 allowances
 INSERT INTO Allowances (allowance_id, allowance_type, amount,status)
