871f5e671c013ca27df34ac51bd7fbd96096d28b
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2023-07-19 21:32:11 +0700

    Major update for payslip

--- HumanResourceApi/Repositories/DailySalaryRepo.cs
+++ HumanResourceApi/Repositories/DailySalaryRepo.cs
@@ -13,5 +13,81 @@ namespace HumanResourceApi.Repositories
             dailySalary = Math.Round(dailySalary, 2);
             return dailySalary;
         }
+        public decimal GetAllowance(string employeeId, DateTime date, decimal dailyAllowance)
+        {
+            var dailySalaryList = GetAll().Where(d => d.EmployeeId == employeeId
+            && d.Date.Year == date.Year && d.Date.Month == date.Month).ToList();
+            if (dailySalaryList is null)
+            {
+                return 0;
+            }
+            decimal allowanceSum = 0;
+            dailySalaryList.ForEach(d =>
+            {
+                allowanceSum += dailyAllowance;
+            });
+            return allowanceSum;
+        }
+        public decimal GetOtSalary(string employeeId, DateTime date)
+        {
+            var dailySalaryList = GetAll().Where(d => d.EmployeeId == employeeId
+            && d.Date.Year == date.Year && d.Date.Month == date.Month).ToList();
+            if (dailySalaryList is null)
+            {
+                return 0;
+            }
+            decimal otSalary = 0;
+            dailySalaryList.ForEach(d =>
+            {
+                otSalary += Math.Round((decimal)d.OtSalary, 2);
+            });
+            return otSalary;
+        }
+        public decimal GetBaseSalary(string employeeId, DateTime date)
+        {
+            var dailySalaryList = GetAll().Where(d => d.EmployeeId == employeeId
+            && d.Date.Year == date.Year && d.Date.Month == date.Month).ToList();
+            if (dailySalaryList is null)
+            {
+                return 0;
+            }
+            decimal baseSalary = 0;
+            dailySalaryList.ForEach(d =>
+            {
+                baseSalary += Math.Round((decimal)d.TotalSalary, 2);
+            });
+            return baseSalary;
+        }
+        public decimal GetMonthlyHour(string employeeId, DateTime date)
+        {
+            var dailySalaryList = GetAll().Where(d => d.EmployeeId == employeeId 
+            && d.Date.Year == date.Year && d.Date.Month == date.Month).ToList();
+            if(dailySalaryList is null)
+            {
+                return 0;
+            }
+            decimal monthlyHour = 0;
+            dailySalaryList.ForEach(d =>
+            {
+                monthlyHour += Math.Round((decimal)d.TotalHours.TotalHours, 2);
+            });
+            return monthlyHour;
+        }
+
+        public decimal GetMonthlyOtHour(string employeeId, DateTime date)
+        {
+            var dailySalaryList = GetAll().Where(d => d.EmployeeId == employeeId
+            && d.Date.Year == date.Year && d.Date.Month == date.Month).ToList();
+            if (dailySalaryList is null)
+            {
+                return 0;
+            }
+            decimal monthlyOtHour = 0;
+            dailySalaryList.ForEach(d =>
+            {
+                monthlyOtHour += Math.Round((decimal)d.OtHours.TotalHours, 2);
+            });
+            return monthlyOtHour;
+        }
     }
 }
--- HumanResourceApi/Repositories/EmployeeBenefitRepo.cs
+++ HumanResourceApi/Repositories/EmployeeBenefitRepo.cs
@@ -5,19 +5,13 @@ namespace HumanResourceApi.Repositories
 {
     public class EmployeeBenefitRepo : BaseRepository.BaseRepository<EmployeeBenefit>
     {
-        public decimal GetAllowanceSum(string employeeId, decimal actualHours)
+        public decimal GetAllowanceSum(string employeeId, decimal dailyAllowanceSum)
         {
-            decimal allowanceSum = 0;
-            decimal actualDate = actualHours / 8;
-            actualDate = Math.Round(actualDate, 2);
+            decimal allowanceSum = dailyAllowanceSum;
             var listBenefit = _dbSet.Include(b => b.Allowance).Where(b => b.EmployeeId == employeeId).ToList();
             listBenefit.ForEach(e =>
             {
-                if (e.Allowance.AllowanceType.ToLower().Equals("daily"))
-                {
-                    allowanceSum += actualDate * (e.Allowance.Amount);
-                }
-                else
+                if (e.Allowance.AllowanceType.ToLower().Equals("monthly"))
                 {
                     allowanceSum += e.Allowance.Amount;
                 }
